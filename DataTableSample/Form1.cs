891465df839dec624cecbd7bf6b3876173706159
ll9 <llwierer@web.de>
2018-09-11 21:05:29 +0200

    Basic Db Editing

--- DataTableSample/Form1.cs
+++ DataTableSample/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,42 @@ namespace DataTableSample
 {
     public partial class Form1 : Form
     {
+        public DataTable DataTable { get; set; } = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = DataTable;
+            LoadDb();
+        }
+
+        public void LoadDb()
+        {
+            using (var connection = GetConnection())
+            {
+                var query = "select * from features";
+                var adapter = new SQLiteDataAdapter(query, connection);
+                adapter.Fill(DataTable);
+            }
+        }
+
+        private static SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(@"Data Source=features.db;");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var connection = GetConnection())
+            {
+                var query = "select * from features";
+                var adapter = new SQLiteDataAdapter(query, connection);
+                var builder = new SQLiteCommandBuilder(adapter);
+
+                adapter.UpdateCommand = builder.GetUpdateCommand();
+                adapter.Update(DataTable);
+            }
         }
     }
+
 }
